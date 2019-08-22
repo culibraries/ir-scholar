e7f2e9ec68139242f823f87aa2514a4df245c4c1
Mark Stacy <mbstacy@gmail.com>
2019-08-22 11:20:12 -0600

    add CU Boulder branding js/css

---
+++ app/assets/javascripts/footer.js
@@ -0,0 +1,16 @@
+// var scriptPram = document.getElementById('ucb-footer-embed');
+// var color = scriptPram.getAttribute('data-color');
+// var logoColor;
+// if (color == 'black') {
+//   logoColor = 'black';
+// }
+// else if (color == 'white') {
+//   logoColor = 'white';
+// }
+// else {
+//   logoColor = 'black';
+// }
+
+
+// var cuFooter = '<div class="ucb-footer"><p><a href="https://www.colorado.edu"><img class="ucb-footer-be-boulder" src="https://cdn.colorado.edu/static/brand-assets/live/images/be-boulder-' + logoColor + '.svg" alt="Be Boulder." style="max-width:240px; height:auto;"/></a></p><p><a class="ucb-home-link" href="https://www.colorado.edu">University of Colorado Boulder</a></p><p>&copy; Regents of the University of Colorado</p><p class="ucb-footer-links"><a href="http://www.colorado.edu/about/privacy-statement">Privacy</a> &bull; <a href="http://www.colorado.edu/about/legal-trademarks">Legal &amp; Trademarks</a> &bull; <a href="http://www.colorado.edu/map">Campus Map</a></p></div>';
+// document.write(cuFooter);
