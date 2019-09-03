732d79cb1233966519ce8a8d1c3b478366749f36
Mark Stacy <mbstacy@gmail.com>
2019-09-03 10:57:49 -0600

    new spec tests for work types

---
+++ spec/controllers/hyrax/working_papers_controller_spec.rb
@@ -0,0 +1,9 @@
+# Generated via
+#  `rails generate hyrax:work WorkingPaper`
+require 'rails_helper'
+
+RSpec.describe Hyrax::WorkingPapersController do
+  it "has tests" do
+    skip "Add your tests here"
+  end
+end
