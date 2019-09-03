3004e88810ba097df34462e16a19a30a48f901e9
Mark Stacy <mbstacy@gmail.com>
2019-09-03 11:00:45 -0600

    working on new metadata model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,15 +1,14 @@
 # Generated via
-#  `rails generate hyrax:work Default`
-class Default < ActiveFedora::Base
+#  `rails generate hyrax:work Book`
+class Book < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
 
-  self.indexer = DefaultIndexer
+  self.indexer = BookIndexer
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
 
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
-  include Scholar::DefaultMetadata
-  #include ::Hyrax::BasicMetadata
+  include ::Hyrax::BasicMetadata
 end
