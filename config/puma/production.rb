608f7666f671b3b1b9e9c234e68ad9f9749a544c
Mark Stacy <mbstacy@gmail.com>
2019-09-09 11:13:02 -0500

    add web server production deployment

---
+++ config/puma/production.rb
@@ -0,0 +1,42 @@
+# Change to match your CPU core count
+workers 2
+
+# Min and Max threads per worker
+threads 1, 6
+
+app_dir = "/data" 
+#File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/tmp"
+
+# Default to production
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
+
+# Set up socket location
+#bind "unix://#{shared_dir}/sockets/puma.sock"
+bind 'tcp://0.0.0.0:3000'
+
+# Logging
+#stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
+
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
+
+# frozen_string_literal:true
+
+#bind 'tcp://0.0.0.0:3000'
+# worker pools kill the capability for ruby-debug-ide integration
+# workers 1
+#preload_app!
+#environment 'development'
+#daemonize false
+# Allow for `touch tmp/restart.txt` to force puma to restart the app
+#plugin :tmp_restart
