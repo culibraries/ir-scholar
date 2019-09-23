b3ceaacfeb1db3c2fbf93066925270b45bd60081
Mark Stacy <mbstacy@gmail.com>
2019-09-23 12:16:57 -0600

    reverted to single instance with puma workers

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,23 +1,34 @@
-# Change to match your CPU core count
-workers 2
 
-# Min and Max threads per worker
-threads 1, 6
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
 
-app_dir = "/data" 
-#File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/tmp"
+#Set Application Directory
+ app_dir = "/data" 
+ shared_dir = "#{app_dir}/tmp"
 
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
-# Set up socket location
-#bind "unix://#{shared_dir}/sockets/puma.sock"
+#daemonize true
 bind 'tcp://0.0.0.0:3000'
 
-# Logging
-#stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked webserver processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory. If you use this option
+# you need to make sure to reconnect any threads in the `on_worker_boot`
+# block.
+#
+ preload_app!
 
 # Set master PID and state locations
 pidfile "#{shared_dir}/pids/puma.pid"
@@ -28,15 +39,5 @@ on_worker_boot do
   require "active_record"
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  puts "Worker Boot!"
 end
-
-# frozen_string_literal:true
-
-#bind 'tcp://0.0.0.0:3000'
-# worker pools kill the capability for ruby-debug-ide integration
-# workers 1
-#preload_app!
-#environment 'development'
-#daemonize false
-# Allow for `touch tmp/restart.txt` to force puma to restart the app
-#plugin :tmp_restart
