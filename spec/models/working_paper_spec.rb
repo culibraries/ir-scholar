c3372236fb9fde5b543bf712238ff06515c64164
Mark Stacy <mbstacy@gmail.com>
2019-09-27 15:22:41 -0600

    deleted worktype files

--- spec/models/working_paper_spec.rb
+++
@@ -1,9 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work WorkingPaper`
-require 'rails_helper'
-
-RSpec.describe WorkingPaper do
-  it "has tests" do
-    skip "Add your tests here"
-  end
-end
