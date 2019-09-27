4f49c1708d68b08f23903c962e593bc5a1d7ee6f
Mark Stacy <mbstacy@gmail.com>
2019-09-27 14:20:09 -0600

    added new Reports WorkType

---
+++ app/forms/hyrax/report_form.rb
@@ -0,0 +1,14 @@
+# Generated via
+require File.expand_path('../../scholar/scholar_work_form.rb', __FILE__)
+#  `rails generate hyrax:work Report`
+module Hyrax
+  # Generated form for Report
+  class ReportForm < Scholar::GeneralWorkForm
+    self.model_class = ::Report
+    # Subtract self.terms 
+    self.terms -= [:conference_location,:conference_name,:date_issued,:has_journal,:has_number,:has_volume,:issn]
+
+    self.required_fields = [:title, :creator,:academic_affiliation, :resource_type, :rights_statement]
+
+  end
+end
