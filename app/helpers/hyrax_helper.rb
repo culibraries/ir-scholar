c13969169966092032b021df03c46db806ee5443
Mark Stacy <mbstacy@gmail.com>
2019-10-02 12:53:19 -0600

    updated helper and search fields

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -2,4 +2,10 @@ module HyraxHelper
   include ::BlacklightHelper
   include Hyrax::BlacklightOverride
   include Hyrax::HyraxHelperBehavior
+
+  def truncated_summary(options)
+    value = options[:value].first
+    value.truncate_words(50)
+  end
+
 end
