fa0191254cb871480cc1d02d1df2b2f3b699a4e2
Mark Stacy <mbstacy@gmail.com>
2019-10-02 20:41:55 -0600

    service and view update

---
+++ app/services/peer_reviewed_service.rb
@@ -0,0 +1,16 @@
+# services/peer_reviewed_service.rb
+
+module PeerReviewedService
+    mattr_accessor :authority
+    self.authority = Qa::Authorities::Local.subauthority_for('peer_reviewed')
+  
+    def self.select_all_options
+      authority.all.map do |element|
+        [element[:label], element[:id]]
+      end
+    end
+  
+    def self.label(id)
+      authority.find(id).fetch('term')
+    end
+end
