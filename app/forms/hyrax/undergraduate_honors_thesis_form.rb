c0b6dafbca6ac3cee638aad3186d900c5ecac1fd
Mark Stacy <mbstacy@gmail.com>
2019-10-14 16:29:32 -0600

    test and commit under grad forms and presenter

--- app/forms/hyrax/undergraduate_honors_thesis_form.rb
+++ app/forms/hyrax/undergraduate_honors_thesis_form.rb
@@ -1,6 +1,6 @@
 # Generated via
 #  `rails generate hyrax:work UndergraduateHonorsThesis`
-# require File.expand_path('../../scholar/scholar_etd_form.rb', __FILE__)
+require File.expand_path('../../scholar/scholar_etd_form.rb', __FILE__)
 module Hyrax
   # Generated form for UndergraduateHonorsThesis
   class UndergraduateHonorsThesisForm < Scholar::EtdWorkForm
