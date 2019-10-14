1567b9b84c37afec5a246ea270ba79ace086f1a5
Mark Stacy <mbstacy@gmail.com>
2019-10-14 10:21:17 -0600

    debug grad etd

--- app/controllers/hyrax/graduate_thesis_or_dissertations_controller.rb
+++ app/controllers/hyrax/graduate_thesis_or_dissertations_controller.rb
@@ -3,6 +3,7 @@
 module Hyrax
   # Generated controller for GraduateThesisOrDissertation
   class GraduateThesisOrDissertationsController < ApplicationController
+    
     # Adds Hyrax behaviors to the controller.
     include Hyrax::WorksControllerBehavior
     include Hyrax::BreadcrumbsForWorks
@@ -10,5 +11,8 @@ module Hyrax
 
     # Use this line if you want to use a custom presenter
     self.show_presenter = Hyrax::GraduateThesisOrDissertationPresenter
+
+    
+
   end
 end
--- app/presenters/hyrax/graduate_thesis_or_dissertation_presenter.rb
+++ app/presenters/hyrax/graduate_thesis_or_dissertation_presenter.rb
@@ -3,11 +3,14 @@
 module Hyrax
   class GraduateThesisOrDissertationPresenter < Hyrax::WorkShowPresenter
     extend ActiveSupport::Concern
-    delegate :title,:abstract,:academic_affiliation,:additional_information,:alt_title,:bibliographic_citation,:conference_location,:conference_name,
+    delegate :abstract,:academic_affiliation,:additional_information,:alt_title,:bibliographic_citation,:conference_location,:conference_name,
     :contributor,:contributor_advisor,:contributor_committeemember,:creator,:date_available,:date_issued,:date_modified,:date_uploaded,
-    :degree_field,:degree_grantors,:degree_name,:depositor,:doi,:editor,:embargo_reason,:file_extent,:file_format,
+    :degree_field,:degree_grantors,:degree_name,:depositor,:doi,:editor,
     :graduation_year,:has_journal,:has_number,:has_volume,:identifier,:in_series,:is_referenced_by,:isbn,:issn,:keyword,
     :language,:license,:location,:other_affiliation,:peerreviewed,:publisher,:replaces,:resource_type,:rights_statement,
     to: :solr_document
   end
 end
+# :embargo_reason,
+# :file_extent,:file_format,
+# :title,
