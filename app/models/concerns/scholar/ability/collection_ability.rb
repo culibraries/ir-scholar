f4112c266d0c1afff6bfe314b83fde5e5568fcd9
Samuel Klopsch <samuelklopsch@gmail.com>
2025-10-28 12:29:33 -0600

    Initial commit

--- app/models/concerns/scholar/ability/collection_ability.rb
+++ app/models/concerns/scholar/ability/collection_ability.rb
@@ -5,11 +5,48 @@ module Scholar
       extend ActiveSupport::Concern
       included do
         def collection_abilities # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+          # models = [::Collection]
+          Hyrax.logger.warn("Collections: #{[::Collection]}")
+          models = [Hyrax::PcdmCollection, Hyrax.config.collection_class].uniq
+          Hyrax.logger.warn("Models: #{models}")
           if admin?
-            can :manage, ::Collection
-            can :manage_any, ::Collection
-            can :create_any, ::Collection
-            can :view_admin_show_any, ::Collection
+            models.each do |collection_model|
+              can :manage, collection_model
+              can :manage_any, collection_model
+              can :create_any, collection_model
+              can :view_admin_show_any, collection_model
+            end
+          else
+            models.each do |collection_model|
+              # can [:edit, :update, :destroy], collection_model do |collection|
+              #   test_edit(collection.id)
+              # end
+
+              can :deposit, collection_model do |collection|
+                Hyrax::Collections::PermissionsService.can_deposit_in_collection?(ability: self, collection_id: collection.id)
+              end
+
+              can :view_admin_show, collection_model do |collection|
+                # admin show page
+                Hyrax::Collections::PermissionsService.can_view_admin_show_for_collection?(ability: self, collection_id: collection.id)
+              end
+
+              can :read, collection_model do |collection|
+                # public show page
+                test_read(collection.id)
+              end
+            end
+            can :deposit, ::SolrDocument do |solr_doc|
+              Hyrax::Collections::PermissionsService.can_deposit_in_collection?(ability: self, collection_id: solr_doc.id) # checks collections and admin_sets
+            end
+            can :deposit, String do |collection_id|
+              Hyrax::Collections::PermissionsService.can_deposit_in_collection?(ability: self, collection_id: collection_id)
+            end
+
+            can :view_admin_show, ::SolrDocument do |solr_doc|
+              # admin show page
+              Hyrax::Collections::PermissionsService.can_view_admin_show_for_collection?(ability: self, collection_id: solr_doc.id) # checks collections and admin_sets
+            end
           end
         end
       end
