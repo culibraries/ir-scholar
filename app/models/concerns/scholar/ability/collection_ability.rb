bb93b83773e7662bd5f206ee1f0e1d7fa95445b9
Samuel Klopsch <samuelklopsch@gmail.com>
2025-10-29 15:18:30 -0600

    Incorporate CSS fix

--- app/models/concerns/scholar/ability/collection_ability.rb
+++ app/models/concerns/scholar/ability/collection_ability.rb
@@ -6,9 +6,7 @@ module Scholar
       included do
         def collection_abilities # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
           # models = [::Collection]
-          Hyrax.logger.warn("Collections: #{[::Collection]}")
           models = [Hyrax::PcdmCollection, Hyrax.config.collection_class].uniq
-          Hyrax.logger.warn("Models: #{models}")
           if admin?
             models.each do |collection_model|
               can :manage, collection_model
@@ -28,7 +26,7 @@ module Scholar
 
               can :view_admin_show, collection_model do |collection|
                 # admin show page
-                Hyrax::Collections::PermissionsService.can_view_admin_show_for_collection?(ability: self, collection_id: collection.id)
+                test_read(collection.id)
               end
 
               can :read, collection_model do |collection|
@@ -36,13 +34,6 @@ module Scholar
                 test_read(collection.id)
               end
             end
-            can :deposit, ::SolrDocument do |solr_doc|
-              Hyrax::Collections::PermissionsService.can_deposit_in_collection?(ability: self, collection_id: solr_doc.id) # checks collections and admin_sets
-            end
-            can :deposit, String do |collection_id|
-              Hyrax::Collections::PermissionsService.can_deposit_in_collection?(ability: self, collection_id: collection_id)
-            end
-
             can :view_admin_show, ::SolrDocument do |solr_doc|
               # admin show page
               Hyrax::Collections::PermissionsService.can_view_admin_show_for_collection?(ability: self, collection_id: solr_doc.id) # checks collections and admin_sets
