45833ff051fba9a466733f02581ea604ee7d18d4
Vida Sabeti <vidasabeti@gmail.com>
2025-11-07 15:15:16 -0700

    Squash merge from maintenance-fix.  Mostly Sam's changes to fix the work's fields that link to query searches or to description of the field.  Also change to header image to not be blurred.  This uses Sam's Docker maintenance fix build due to Clamav version not pulling after Oct 2025.

--- app/models/concerns/scholar/ability/collection_ability.rb
+++ app/models/concerns/scholar/ability/collection_ability.rb
@@ -5,11 +5,39 @@ module Scholar
       extend ActiveSupport::Concern
       included do
         def collection_abilities # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+          # models = [::Collection]
+          models = [Hyrax::PcdmCollection, Hyrax.config.collection_class].uniq
           if admin?
-            can :manage, ::Collection
-            can :manage_any, ::Collection
-            can :create_any, ::Collection
-            can :view_admin_show_any, ::Collection
+            models.each do |collection_model|
+              can :manage, collection_model
+              can :manage_any, collection_model
+              can :create_any, collection_model
+              can :view_admin_show_any, collection_model
+            end
+          else
+            models.each do |collection_model|
+              # can [:edit, :update, :destroy], collection_model do |collection|
+              #   test_edit(collection.id)
+              # end
+
+              can :deposit, collection_model do |collection|
+                Hyrax::Collections::PermissionsService.can_deposit_in_collection?(ability: self, collection_id: collection.id)
+              end
+
+              can :view_admin_show, collection_model do |collection|
+                # admin show page
+                test_read(collection.id)
+              end
+
+              can :read, collection_model do |collection|
+                # public show page
+                test_read(collection.id)
+              end
+            end
+            can :view_admin_show, ::SolrDocument do |solr_doc|
+              # admin show page
+              Hyrax::Collections::PermissionsService.can_view_admin_show_for_collection?(ability: self, collection_id: solr_doc.id) # checks collections and admin_sets
+            end
           end
         end
       end
