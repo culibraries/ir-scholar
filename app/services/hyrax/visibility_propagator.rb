813f46d6b2e290a7266fce25c4ec6c7671e5ee87
Samuel Klopsch <samuelklopsch@gmail.com>
2026-03-12 15:25:30 -0600

    Fix the visibility copy job issue by updating the propogator service to Hyrax 5.x instead of working around the 3.x model agnostic code

---
+++ app/services/hyrax/visibility_propagator.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+## CUBL change:
+# Copy this service from Hyrax 5.x to override the 3.x version we use for compatibility between the current ActiveFedora
+# models we use and the agnostic code introduced with Valkyrie (the agnostic layers don't work at the moment for us in
+# Hyrax 3.x)
+module Hyrax
+  ##
+  # @abstract Propagates visibility from a provided object (e.g. a Work) to some
+  #   group of its members (e.g. file_sets).
+  class VisibilityPropagator
+    ##
+    # @param source [#visibility] the object to propagate visibility from
+    #
+    # @return [#propagate]
+    def self.for(source:)
+      case source
+      when ActiveFedora::Base # ActiveFedora
+        FileSetVisibilityPropagator.new(source: source)
+      when Hyrax::Resource # Valkyrie
+        # Due to performance issues for a Hyrax::Resource in Fedora 6,
+        # use a job that copies both permissions and visibility instead
+        # of reloading and iterating again.
+        ResourcePermissionsVisibilityPropagator.new(source: source)
+      else
+        NullVisibilityPropagator.new(source: source)
+      end
+    end
+
+    ##
+    # Provides a null/logging implementation of the visibility propagator.
+    class NullVisibilityPropagator
+      ##
+      # @!attribute [rw] source
+      #   @return [#visibility]
+      attr_accessor :source
+
+      ##
+      # @param source [#visibility] the object to propagate visibility from
+      def initialize(source:)
+        self.source = source
+      end
+
+      ##
+      # @return [void]
+      # @raise [RuntimeError] if we're in development mode
+      def propagate
+        message = "Tried to propagate visibility to members of #{source} " \
+          "but didn't know what kind of object it is. Model " \
+          "name #{source.try(:model_name)}. Called from #{caller[0]}."
+
+        Hyrax.logger.warn(message)
+        Rails.env.development? ? raise(message) : :noop
+      end
+    end
+  end
+end
